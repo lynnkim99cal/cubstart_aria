8ff8d07055497362cecc7ae4d03b5fac3e601317
Yanyi Chen <ariachen@berkeley.edu>
2022-03-21 19:14:44 -0700

    final submission

---
+++ hw4/waitlisthelper-skeleton/feedbackModel.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let feedbackModel =
+    [(90, "You're good 😄"),
+     (60, "You're probably okay ☺️"),
+     (40, "Idk, it's like 50-50 😐"),
+     (20, "You're probably not getting off 😕"),
+     (0, "Rip 😔")]
+
+var test = "test"
